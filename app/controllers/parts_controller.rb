39663096bb86f85024cdafd878c3bd646f054ef4
DAVID ZHU <DAVID.C.ZHU@GMAIL.COM>
2015-06-13 20:37:51 -0400

    Search bar route

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -8,11 +8,11 @@ class PartsController < ActionController::Base
         struct = Struct.new(:id, :version, :downloads, :summary)
 
         a = struct.new("Testing Framework", "0.0.0", 23425, "A testing framework")
-        b = struct.new("Testing Framework", "0.0.0", 23425, "A testing framework")
-        c = struct.new("Testing Framework", "0.0.0", 23425, "A testing framework")
-        d = struct.new("Testing Framework", "0.0.0", 23425, "A testing framework")
-        e = struct.new("Testing Framework", "0.0.0", 23425, "A testing framework")
-        f = struct.new("Testing Framework", "0.0.0", 23425, "A testing framework")
+        b = struct.new("Runescape", "3.7.110", 3513546, "A game")
+        c = struct.new("MaQuan", "0.0.0", 1, "a monster")
+        d = struct.new("Testing Framework 2", "0.1.0", 515, "A testing framework 2")
+        e = struct.new("Testing Framework 3", "3.0.0", 456, "A testing framework 3")
+        f = struct.new("Testing Framework 4", "0.0.5", 234534, "A testing framework 4")
 
         @items = [a,b,c,d,e,f]
 	end
@@ -21,5 +21,8 @@ class PartsController < ActionController::Base
     end
 
     def oldversion
+    end
+
+    def search
     end
 end
