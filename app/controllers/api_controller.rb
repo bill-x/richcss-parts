2a18a7d49ee63c0a8c126faad8fd276e23588947
DAVID ZHU <DAVID.C.ZHU@GMAIL.COM>
2016-01-24 14:52:54 -0500

    stuffs

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,4 +8,14 @@ class ApiController < ActionController::Base
             render :json => part, status: 200
         end
 	end
+
+  def getPartWithVersion
+        part = Version.where(:name => params[:part_name], :version => params[:version]).first
+
+        if part.nil?
+            render :text ="Part: #{params[:part_name]} with version #{params[:version]} does not exist.", status: 400
+        else
+            render :json => part, status: 200
+        end
+  end
 end
