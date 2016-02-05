59ba6cc3d7489f1721e73d93d532bdea61f4887d
Bill Xu <billxu93@gmail.com>
2016-02-05 16:52:33 -0500

    fix dependency api

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120025942) do
+ActiveRecord::Schema.define(version: 20160205212205) do
 
   create_table "contributors", force: true do |t|
     t.string   "part_name"
